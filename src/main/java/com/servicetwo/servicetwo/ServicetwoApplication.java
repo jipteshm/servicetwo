f7f97ee7f6765d59aeff89be38bfa3b6de38e104
Jiptesh Moharana <jiptesh.moharana@iddinkgroup.com>
2018-10-15 20:03:43 +0530

    commit to service two

---
+++ src/main/java/com/servicetwo/servicetwo/ServicetwoApplication.java
@@ -0,0 +1,12 @@
+package com.servicetwo.servicetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicetwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicetwoApplication.class, args);
+	}
+}
